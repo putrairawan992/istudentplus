5c34743ce688709faf5f4cef507fc7c4cf30611c
PUTRA IRAWAN <putra.irawan@gunanusa.co.id>
2026-08-24 11:11:30 +0700

    feat: add Google Tag Manager (GTM-5DC3QD86)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import FloatWhatsApp from "./components/FloatWhatsApp";
 import { SITE_URL as siteUrl } from "../lib/site";
@@ -51,8 +52,23 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
         />
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5DC3QD86');`}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col font-sans">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5DC3QD86"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(){
